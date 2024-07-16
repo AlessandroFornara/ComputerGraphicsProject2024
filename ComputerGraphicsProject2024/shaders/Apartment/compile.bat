f9299bc7aa4eb66f39d46d1f3afa60c3a36a801b
RiccardoFigini <riccardo.figini@mail.polimi.it>
2024-07-16 12:06:13 +0200

    Update shader apartment

--- ComputerGraphicsProject2024/shaders/Apartment/compile.bat
+++ ComputerGraphicsProject2024/shaders/Apartment/compile.bat
@@ -1,2 +1,2 @@
 C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Vert.vert -o Vert.spv
-C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Frag.frag -o ToonLight.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Frag.frag -o Frag.spv
