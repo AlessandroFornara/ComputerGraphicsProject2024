b785956a239b164f4b75718729897bc9cd921174
Alex <alessandro.fornara01@gmail.com>
2024-08-31 18:53:43 +0200

    Added compileAll.bat

---
+++ ComputerGraphicsProject2024/shaders/compileAll.bat
@@ -0,0 +1,11 @@
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Apartment/ApartmentShader.vert -o Apartment/ApartmentVert.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Apartment/ApartmentShader.frag -o Apartment/ApartmentFrag.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe City/CityShader.vert -o City/CityVert.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe City/CityShader.frag -o City/CityFrag.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Emission/EmissionShader.vert -o Emission/generalEmissionVert.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Emission/EmissionShader.frag -o Emission/generalEmissionFrag.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Shop/ShopShader.vert -o Shop/ShopVert.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe Shop/ShopShader.frag -o Shop/ShopSpotLight.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe TextShader.vert -o TextVert.spv
+C:\VulkanSDK\1.3.275.0\Bin\glslc.exe TextShader.frag -o TextFrag.spv
+pause
